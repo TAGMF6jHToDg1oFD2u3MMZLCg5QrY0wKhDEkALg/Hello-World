f327e57f0d7582e29141d9adaa880ad82143356b
Mike Martin <chachiisgod@protonmail.com>
2020-12-22 09:33:02 -0500

    First commit

---
+++ Hello_World.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let constant = 10
+var y = 10
+var x = y + constant
+
+var message = "The best way to get started is to stop talking and code."
+
+var greeting = "Hello "
+var name = "Simon"
+var new_message = greeting + name
+message.uppercased()
