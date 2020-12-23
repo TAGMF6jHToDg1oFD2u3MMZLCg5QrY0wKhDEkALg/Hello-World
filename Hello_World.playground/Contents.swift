8a08bf5d40d58a6b13d64bf78393aa46c4c31541
Mike Martin <chachiisgod@protonmail.com>
2020-12-23 08:37:03 -0500

    Ready for Chapter 3

--- Hello_World.playground/Contents.swift
+++ Hello_World.playground/Contents.swift
@@ -7,13 +7,13 @@ var y = 10
 var x = y + constant
 
 var message = "The best way to get started is to stop talking and code."
+message.uppercased()
+message.lowercased()
+message.count
 
 var greeting = "Hello "
 var name = "Simon"
 var new_message = greeting + name
-message.uppercased()
-message.lowercased()
-message.count
 
 var bookPrice = 39
 var numOfCopies = 5
@@ -35,4 +35,23 @@ var emojiDict: [String: String] = ["👻": "Ghost",
 var wordToLookup = "👻"
 var meaning = emojiDict[wordToLookup]
 
-var jobTitle: String?
+if let meaning = meaning
+{
+    print(meaning)
+}
+
+let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+containerView.backgroundColor = UIColor.magenta
+
+let emojiLabel = UILabel(frame: CGRect(x: 95, y: 20, width: 150, height: 150))
+emojiLabel.text = wordToLookup
+emojiLabel.font = UIFont.systemFont(ofSize: 100.0)
+
+containerView.addSubview(emojiLabel)
+
+let meaningLabel = UILabel(frame: CGRect(x: 110, y: 100, width: 150, height: 150))
+meaningLabel.text = meaning
+meaningLabel.font = UIFont.systemFont(ofSize: 30.0)
+meaningLabel.textColor = UIColor.white
+
+containerView.addSubview(meaningLabel)
