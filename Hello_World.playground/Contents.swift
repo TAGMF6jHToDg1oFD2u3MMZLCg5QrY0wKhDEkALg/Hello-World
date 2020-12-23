cf0291056f9ca517a52c12b974222ff37b5a2329
Mike Martin <chachiisgod@protonmail.com>
2020-12-23 07:42:03 -0500

    Test commit

--- Hello_World.playground/Contents.swift
+++ Hello_World.playground/Contents.swift
@@ -12,3 +12,27 @@ var greeting = "Hello "
 var name = "Simon"
 var new_message = greeting + name
 message.uppercased()
+message.lowercased()
+message.count
+
+var bookPrice = 39
+var numOfCopies = 5
+var totalPrice = bookPrice * numOfCopies
+var totalPriceMessage = "The price of the book is $\(totalPrice)"
+
+var bookCollectionDict = ["1328683788": "Tool of Titans", "0307463745": "Rework", "1612060919": "Authority"]
+
+for (key, value) in bookCollectionDict
+{
+    print("ISBN: \(key)")
+    print("Title: \(value)")
+}
+var emojiDict: [String: String] = ["👻": "Ghost",
+                                   "💩": "Poop",
+                                   "🤯": "Angry",
+                                   "😱": "Scream"]
+
+var wordToLookup = "👻"
+var meaning = emojiDict[wordToLookup]
+
+var jobTitle: String?
